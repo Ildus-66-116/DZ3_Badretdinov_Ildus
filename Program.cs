bd8647dc50786d94c5cec04a5138a5da2b2699d9
ILDUS <ildus6696@gmail.com>
2023-05-30 21:21:37 +0300

    Reshena Zadacha 23

--- Program.cs
+++ Program.cs
@@ -26,18 +26,32 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// Console.Clear();
+// Console.Write("Координату X(1): ");
+// double x1 = double.Parse(Console.ReadLine()!);
+// Console.Write("Координату Y(1): ");
+// double y1 = double.Parse(Console.ReadLine()!);
+// Console.Write("Координату Z(1): ");
+// double z1 = double.Parse(Console.ReadLine()!);
+// Console.Write("Координату X(2): ");
+// double x2 = double.Parse(Console.ReadLine()!);
+// Console.Write("Координату Y(2): ");
+// double y2 = double.Parse(Console.ReadLine()!);
+// Console.Write("Координату Z(2): ");
+// double z2 = double.Parse(Console.ReadLine()!);
+// double S = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.WriteLine($"Расстояние равно {Math.Round(S, 2)}");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
 Console.Clear();
-Console.Write("Координату X(1): ");
-double x1 = double.Parse(Console.ReadLine()!);
-Console.Write("Координату Y(1): ");
-double y1 = double.Parse(Console.ReadLine()!);
-Console.Write("Координату Z(1): ");
-double z1 = double.Parse(Console.ReadLine()!);
-Console.Write("Координату X(2): ");
-double x2 = double.Parse(Console.ReadLine()!);
-Console.Write("Координату Y(2): ");
-double y2 = double.Parse(Console.ReadLine()!);
-Console.Write("Координату Z(2): ");
-double z2 = double.Parse(Console.ReadLine()!);
-double S = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine($"Расстояние равно {Math.Round(S, 2)}");
+Console.Write("Ведите число: ");
+int x = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= x; i++)
+    {
+    double s = Math.Pow(i, 3);
+    Console.Write ($" {s}");
+    }
